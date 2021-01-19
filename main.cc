6db3f0adfce3d9d69653e38654f0880eea9c82cc
Dillon F <54560136+pensiveparrot@users.noreply.github.com>
2021-01-18 20:24:46 -0500

    Update main.cc
    markup code, delete unnecessary buggy code

--- main.cc
+++ main.cc
@@ -2,131 +2,111 @@
 #include "debug.h"
 bool VDIFile::VDIOpen(char *fn)
 {
-    
-    this->fd=open(fn,O_RDONLY);
+
+    this->fd = open(fn, O_RDONLY);
     if(this->fd == -1)
         return false;
     return true;
 }
 void VDIFile::VDIClose(int fd)
 {
-	fd=this->fd;
+    fd = this->fd;
     close(fd);
 }
 ssize_t VDIFile::VDIRead(int fd, void *buf, size_t count)
 {
-	fd=this->fd;
-	return read(fd, buf, count);
+    fd = this->fd;
+    return read(fd, buf, count);
 
 }
 ssize_t VDIFile::VDIWrite(int fd, void *buf, size_t count)
 {
-	fd=this->fd;
-    /*ssize_t curbyte;
-    size_t byteslefttowrite = 0;
-    while(byteslefttowrite != count)
-    {
-        curbyte = write(this->fd, &buf, byteslefttowrite);
-        byteslefttowrite++;
-    }
-    return curbyte;*/
-    //lseek(this->fd,
-    VDISeek(fd,0,0);
-    return write(fd,buf,count);
+    fd = this->fd;
+    return write(fd, buf, count);
 }
 off_t VDIFile::VDISeek(int fd, off_t offset, int anchor)
 {
-    fd=this->fd;
+    fd = this->fd;
     off_t location;
-/*    if(anchor == SEEK_SET)
+    switch(anchor)
     {
-        location = lseek(this->fd, offset, anchor);
+    case(SEEK_SET):
+        location = lseek(fd, offset, anchor);
         if(location < 0) return 1;
         this->cursor = location;
-    }
-    if(anchor == SEEK_CUR)
-    {
-        location = lseek(this->fd, offset, anchor);
+        break;
+    case(SEEK_CUR):
+        location = lseek(fd, offset, anchor);
         if(location < 0) return 1;
         this->cursor += offset;
-    }
-    if(anchor == SEEK_END)
-    {
-        location = lseek(this->fd, offset, anchor);
+        break;
+    case(SEEK_END):
+        location = lseek(fd, offset, anchor);
         if(location < 0) return 1;
         this->cursor = offset + this->fileSize;
-    }*/
-    switch(anchor){
-    case(SEEK_SET):
-    	location=lseek(fd,offset,anchor);
-    	if(location<0) return 1;
-    	this->cursor=location;
-    	break;
-    case(SEEK_CUR):
-    	location=lseek(fd,offset,anchor);
-    	if(location<0) return 1;
-    	this->cursor+=offset;
-    	break;
-    	case(SEEK_END):
-    	location=lseek(fd,offset,anchor);
-    	if(location<0) return 1;
-    	this->cursor=offset+this->fileSize;
-    	break;
+        break;
     default:
-    perror("seek");
-    break;
-    	
+        perror("seek");
+        break;
+
     }
     return this->cursor;
-   
+
     //return lseek(fd,offset,anchor);
 }
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     debug dbg;
-    void* buf;
+    void *buf;
     VDIFile VDI;
-    bool ab=VDI.VDIOpen(argv[1]);
-    if(ab==true)
+    bool ab = VDI.VDIOpen(argv[1]);
+    if(ab == true)
     {
-    buf=malloc(1024);
-    VDI.VDISeek(VDI.fd,0,SEEK_SET);
-    //lseek(VDI.fd,0,SEEK_SET);
-    //read(VDI.fd,buf,1024);
-   VDI.VDIRead(VDI.fd,buf,1024);
-    dbg.displayBuffer((uint8_t*)buf,1024,0);
-    VDI.VDIClose(VDI.fd);
+        buf = malloc(1024);
+        VDI.VDISeek(VDI.fd, 0, SEEK_SET);
+        VDI.VDIRead(VDI.fd, buf, 1024);
+        dbg.displayBuffer((uint8_t *)buf, 1024, 0);
+        VDI.VDIClose(VDI.fd);
     }
-    
+
     return 0;
 }
-void debug::displayBufferPage(uint8_t *buf, uint32_t count, uint32_t skip, uint64_t offset){
-	int c=0;
-	for(int i=skip;i<=skip+count;i++){
-	if(skip<=i&&i<skip+count){
-	uint8_t bytes=buf[i];
-	std::cout<<std::setfill('0')<<std::setw(2)<<std::hex<<(int)bytes<<" ";
-	}
-	else
-	{
-	std::cout<<std::endl;
-	break;
-	}
-	
-	}
-	c++;
-	if(c%16==0)
-	std::cout<<std::endl;
-	    for (int j = skip; j <= count; j++) {
-        if (skip <= j && j < skip + count) {
+void debug::displayBufferPage(uint8_t *buf, uint32_t count, uint32_t skip, uint64_t offset)
+{
+    int c = 0;
+    for(int i = skip; i <= skip + count; i++)
+    {
+        if(skip <= i && i < skip + count)
+        {
+            uint8_t bytes = buf[i];
+            std::cout << std::setfill('0') << std::setw(2) << std::hex << (int)bytes << " ";
+        }
+        else
+        {
+            std::cout << std::endl;
+            break;
+        }
+
+    }
+    c++;
+    if(c % 16 == 0)
+        std::cout << std::endl;
+    for (int j = skip; j <= count; j++)
+    {
+        if (skip <= j && j < skip + count)
+        {
             uint8_t bytes = buf[j];
-            if (isprint(int(bytes))) {
+            if (isprint(int(bytes)))
+            {
                 std::cout << bytes << " ";
-            } else {
+            }
+            else
+            {
                 std::cout << " ";
             }
         }
-        else{
+        else
+        {
             std::cout << std::endl;
             break;
         }
