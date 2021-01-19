03d41b786947d090cb55698516ef7e6bf02f91ed
zbx <unidlinghost@gmail.com>
2021-01-19 18:36:22 -0500

    vdi functs~VDIWrite does not work

--- main.cc
+++ main.cc
@@ -1,48 +1,59 @@
+//#include <cstddef>
 #include "vdifile.h"
 #include "debug.h"
-bool VDIFile::VDIOpen(char *fn)
+
+
+VDIFile::VDIFile(int tmapsize) {
+    transmapsize = tmapsize;
+    transmapptr = new int[transmapsize];
+    cursor = 0;
+}
+int VDIFile::VDIOpen(char *fn)
 {
 
+  //struct vdifile * file= (struct VDIFile *) malloc(sizeof(struct VDIFile));
     this->fd = open(fn, O_RDONLY);
-    if(this->fd == -1)
+    
+    if(fd == -1)
         return false;
-    return true;
+    else{
+    this->transmapsize = header.cBlocks;
+    //lseek(fd, 0, SEEK_SET);
+    read(this->fd, &header, sizeof(header));    
+    this->map = new int[transmapsize];
+    lseek(this->fd, header.offBlocks, SEEK_SET);
+    read(this->fd, map, transmapsize*sizeof(int));
+   
+
+}
+ return this->fd;
+this->cursor=0;
 }
 void VDIFile::VDIClose(int fd)
-{
-    fd = this->fd;
+{	
+fd=this->fd;
     close(fd);
-}
-ssize_t VDIFile::VDIRead(int fd, void *buf, size_t count)
-{
-    fd = this->fd;
-    return read(fd, buf, count);
-
-}
-ssize_t VDIFile::VDIWrite(int fd, void *buf, size_t count)
-{
-    fd = this->fd;
-    return write(fd, buf, count);
 }
 off_t VDIFile::VDISeek(int fd, off_t offset, int anchor)
 {
+    whence=anchor;
     fd = this->fd;
     off_t location;
-    switch(anchor)
+    switch(whence)
     {
     case(SEEK_SET):
-        location = lseek(fd, offset, anchor);
-        if(location < 0) return 1;
-        this->cursor = location;
+        //location = lseek(fd, offset, anchor);
+        //if(location < 0) return 1;
+        this->cursor = offset;
         break;
     case(SEEK_CUR):
-        location = lseek(fd, offset, anchor);
-        if(location < 0) return 1;
+        //location = lseek(fd, offset, anchor);
+        //if(location < 0) return 1;
         this->cursor += offset;
         break;
     case(SEEK_END):
-        location = lseek(fd, offset, anchor);
-        if(location < 0) return 1;
+        //location = lseek(fd, offset, anchor);
+        //if(location < 0) return 1;
         this->cursor = offset + this->fileSize;
         break;
     default:
@@ -54,18 +65,116 @@ off_t VDIFile::VDISeek(int fd, off_t offset, int anchor)
 
     //return lseek(fd,offset,anchor);
 }
+ssize_t VDIFile::VDIRead(int fd, void *buf, size_t count)
+{
+     
+    size_t bytesleft = count,
+
+     bytesread=0,
+    virtualpage,
+offset,
+ physicalpage,
+reallocation;
+fd=this->fd;
+    while(bytesleft > 0) {
+       virtualpage = cursor/this->header.cbBlock;
+       offset = cursor%this->header.cbBlock;
+       physicalpage = this->transmapptr[virtualpage];
+      reallocation = physicalpage*this->header.cbBlock + offset;
+      lseek(fd, reallocation + this->header.offData, SEEK_SET);
+      ssize_t bytesjustread=0;
+	if(reallocation>=0)
+	{
+	size_t bytestoread=0;
+	if(count<this->header.cbBlock)
+	bytestoread=count;
+	else
+	bytestoread=this->header.cbBlock;
+	
+	bytesjustread=read(fd,(uint8_t*)buf+bytesread,bytestoread);
+	}
+	else
+	{
+	for(int i=0;i>this->header.cbBlock;i++){
+	 *(static_cast<uint8_t *>(buf) + (bytesread + bytesjustread)) = '0';
+	bytesjustread++;
+	}
+	}
+      	if(bytesjustread<0)
+      	return -1;
+      	bytesread+=bytesjustread;
+      	bytesleft-=bytesjustread;
+      	VDISeek(fd,bytesjustread,SEEK_CUR);
+        
+    }
+
+    return bytesread;
+
+
+}
+
+ssize_t VDIFile::VDIWrite(int fd, void *buf, size_t count)
+{	
+	fd=this->fd;
+	ssize_t byte;
+	size_t bytesleft=count;
+	while(bytesleft>0)
+	{
+	byte=write(fd,buf,bytesleft);
+	bytesleft--;
+	}
+	
+    return byte;
+}
+
 int main(int argc, char *argv[])
 {
     debug dbg;
-    void *buf;
-    VDIFile VDI;
+    uint8_t buf[4096];
+    VDIFile VDI(512);
     bool ab = VDI.VDIOpen(argv[1]);
     if(ab == true)
     {
-        buf = malloc(1024);
+        //buf[4096];
         VDI.VDISeek(VDI.fd, 0, SEEK_SET);
-        VDI.VDIRead(VDI.fd, buf, 1024);
-        dbg.displayBuffer((uint8_t *)buf, 1024, 0);
+        //lseek(VDI.fd,0,SEEK_SET);
+        //read(VDI.fd,buf,4096);
+        VDI.VDIRead(VDI.fd, buf, 4096);
+        /*std::cout<<"dump of VDI Header"<<std::endl;
+        std::cout<<"Image Name:"<<VDI.header.szFileInfo<<std::endl;
+         printf("Signature: 0x%08x\n",VDI.header.u32Signature);
+        std::cout<<"version:"<<VDI.header.u32Version<<std::endl;
+        std::cout<<"version:"<<VDI.header.szComment<<std::endl;*/
+        
+    printf("Image name: [%-64.64s]\n",VDI.header.szFileInfo);
+    printf("Signature: 0x%08x\n",VDI.header.u32Signature);
+    printf("Version: 0x%08x\n",VDI.header.u32Version);
+    std::cout<<"Header size: 0x"<<VDI.header.cbHeader<<std::endl;
+    printf("Image type: 0x%08x\n",VDI.header.u32Type);
+    printf("Flags: 0x%08x\n",VDI.header.fFlags);
+   /* printf("Virtual CHS: %d-%d-%d\n",VDI.header.cCylinders,
+           VDI.header.cHeads,VDI.header.cSectors);*/
+           std::cout<<"Sector Size:"<<
+           VDI.header.cbSector<<std::endl;
+    /*
+    printf("Map offset: 0x%08x\n",VDI.header.offBlocks);*/
+    /*printf("    Frame offset: 0x%08x  %d\n",f->header->dataOffset,
+           f->header->dataOffset);
+    printf("      Frame size: 0x%08x  %d\n",f->header->pageSize,
+           f->header->pageSize);
+    printf("Extra frame size: 0x%08x  %d\n",f->header->extraPageSize,
+           f->header->extraPageSize);
+    printf("    Total frames: 0x%08x  %d\n",f->header->nPagesTotal,
+           f->header->nPagesTotal);
+    printf("Frames allocated: 0x%08x  %d\n",f->header->nPagesAllocated,
+           f->header->nPagesAllocated);*/
+    std::cout<<"Disk Size: "<<VDI.header.cbDisk<<std::endl;
+    /*printf("UUID: %s\n",uuid2ascii(&VDI.header.uuidCreate));
+    printf("Last snap UUID: %s\n",uuid2ascii(&VDI.header.uuidModify));
+    printf("Link UUID: %s\n",uuid2ascii(&VDI.header.uuidPrevImage));
+    printf("Parent UUID: %s\n",uuid2ascii(&VDI.header.uuidPrevImageModify));*/
+    printf("Image comment:\n");
+        dbg.displayBuffer((uint8_t *)buf, 4096, 0);
         VDI.VDIClose(VDI.fd);
     }
 
@@ -73,6 +182,7 @@ int main(int argc, char *argv[])
 }
 void debug::displayBufferPage(uint8_t *buf, uint32_t count, uint32_t skip, uint64_t offset)
 {
+    std::cout<<"value in hex\n";
     int c = 0;
     for(int i = skip; i <= skip + count; i++)
     {
@@ -91,6 +201,8 @@ void debug::displayBufferPage(uint8_t *buf, uint32_t count, uint32_t skip, uint6
     c++;
     if(c % 16 == 0)
         std::cout << std::endl;
+        
+    std::cout<<"value in ascii\n";
     for (int j = skip; j <= count; j++)
     {
         if (skip <= j && j < skip + count)
@@ -98,7 +210,7 @@ void debug::displayBufferPage(uint8_t *buf, uint32_t count, uint32_t skip, uint6
             uint8_t bytes = buf[j];
             if (isprint(int(bytes)))
             {
-                std::cout << bytes << " ";
+                std::cout << bytes << "";
             }
             else
             {
@@ -112,6 +224,7 @@ void debug::displayBufferPage(uint8_t *buf, uint32_t count, uint32_t skip, uint6
         }
     }
 }
+
 
 void debug::displayBuffer(uint8_t *buf, uint32_t count, uint64_t offset)
 {
--- vdifile.h
+++ vdifile.h
@@ -1,15 +1,103 @@
 #pragma once
 #include <fcntl.h>
 #include <unistd.h>
+#include <cstring>
+#include <iostream>
+#include <iomanip>
+struct VDIHEADER
+{
+/** Just text info about image type, for eyes only. */
+    char            szFileInfo[64];
+    /** The image signature (VDI_IMAGE_SIGNATURE). */
+    uint32_t        u32Signature;
+    /** The image version (VDI_IMAGE_VERSION). */
+    uint32_t        u32Version;
+   
+    /** The image type (VDI_IMAGE_TYPE_*). */
+    uint32_t        u32Type;
+    /** Image flags (VDI_IMAGE_FLAGS_*). */
+    uint32_t        fFlags;
+    /** Image comment. (UTF-8) */
+    char            szComment[256];
+    /** Legacy image geometry (previous code stored PCHS there). */
+    /** Size of disk (in bytes). */
+    uint64_t        cbDisk;
+    /** Block size. (For instance VDI_IMAGE_BLOCK_SIZE.) */
+    uint32_t        cbBlock;
+    /** Number of blocks. */
+    uint32_t        cBlocks;
+    /** Number of allocated blocks. */
+    uint8_t 	translationmap[256];
+    uint32_t    cBlocksAllocated;
+    /** UUID of image. */
+   // char          uuidCreate;
+    /** UUID of image's last modification. */
+    //RTUUID          uuidModify;
+    /** Only for secondary images - UUID of primary image. */
+   // RTUUID          uuidLinkage;
+    /** Size of this structure in bytes. */
+    uint32_t        cbHeader;
+    /** The image type (VDI_IMAGE_TYPE_*). */
+   // uint32_t        u32Type;
+    /** Image flags (VDI_IMAGE_FLAGS_*). */
+   // uint32_t        fFlags;
+    /** Image comment. (UTF-8) */
+   // char            szComment[256];
+    /** Offset of Blocks array from the beginning of image file.
+     * Should be sector-aligned for HDD access optimization. */
+    uint32_t        offBlocks;
+    /** Offset of image data from the beginning of image file.
+     * Should be sector-aligned for HDD access optimization. */
+    uint32_t        offData;
+    /** Legacy image geometry (previous code stored PCHS there). */
+  //  VDIDISKGEOMETRY LegacyGeometry;
+  /** Cylinders. */
+    uint32_t    cCylinders;
+    uint32_t cHeads;
+    uint32_t    cSectors;
+    /** Sector size. (bytes per sector) */
+    uint32_t    cbSector;
+    /** Was BIOS HDD translation mode, now unused. */
+    uint32_t        u32Dummy;
+    /** Size of disk (in bytes). */
+   // uint64_t        cbDisk;
+    /** Block size. (For instance VDI_IMAGE_BLOCK_SIZE.) Should be a power of 2! */
+    //uint32_t        cbBlock;
+    /** Size of additional service information of every data block.
+     * Prepended before block data. May be 0.
+     * Should be a power of 2 and sector-aligned for optimization reasons. */
+    uint32_t        cbBlockExtra;
+    /** Number of blocks. */
+    //uint32_t        cBlocks;
+    /** Number of allocated blocks. */
+    //uint32_t        cBlocksAllocated;
+    /** UUID of image. */
+    char          uuidCreate[16];
+    /** UUID of image's last modification. */
+    char          uuidModify[16];
+    /** Only for secondary images - UUID of previous image. */
+    char          uuidLinkage[16];
+    /** Only for secondary images - UUID of previous image's last modification. */
+    char          uuidParentModify[16];
+} VDIHEADER, *PVDIHEADER1;
+
 class VDIFile{
+
 public:
-bool VDIOpen(char* fn);
+int VDIOpen(char* fn);
+VDIFile(int);
 void VDIClose(int fd);
 ssize_t VDIRead(int fd,void* buf,size_t count);
 ssize_t VDIWrite(int fd,void* buf,size_t count);
 off_t VDISeek(int fd,off_t offset,int anchor);
+struct VDIHEADER header;
 int fileSize;
 off_t cursor;
-int file;
+int transmapsize;
+int* transmapptr;
+//int file;
 int fd;
+int* map;
+int whence;
 };
+
